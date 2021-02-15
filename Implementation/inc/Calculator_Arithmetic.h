6f000b03f10462061cf55138a6b18faf933b7302
99003747 <78856378+99003747@users.noreply.github.com>
2021-02-15 10:30:09 +0530

    updated code

--- Implementation/inc/Calculator_Arithmetic.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CALCULATOR_ARITHMETIC_H__
-#define __CALCULATOR_ARITHMETIC_H__
-
-#include<stdio.h>
-
-void calculator_arithmetic();
-
-#endif
---
+++ Implementation/inc/equation_operation.h
@@ -0,0 +1,10 @@
+#ifndef __EQUATION_OPERATION_H__
+#define __EQUATION_OPERATION_H__
+
+#include<stdio.h>
+#include<math.h>
+
+
+int equation(float,float,float);
+
+#endif
