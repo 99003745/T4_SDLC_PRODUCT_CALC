f8ca168c7293af7c6bd5f7075f50cd8aa00179b7
99003748 <78853340+99003748@users.noreply.github.com>
2021-02-15 10:23:12 +0530

    code update

---
+++ Implementation/inc/roots_cubes.h
@@ -0,0 +1,10 @@
+/** 
+* @file calculator_operations.h
+* Calculator application with 4 mathematical operations
+*
+*/
+#ifndef __ROOTS_CUBES_H__
+#define __ROOTS_CUBES_H__
+
+#include <stdio.h>
+#include <math.h>
--- Implementation/src/roots_cubes.c
+++ Implementation/src/roots_cubes.c
@@ -1,18 +1,60 @@
-#include <stdio.h>
-#include <math.h>
+#include <roots_cubes.h>
  
 int main()
 {
-    int i,n;
- 
-    printf("Enter the value of N: ");
-    scanf("%d",&n);
- 
-    printf("No     Square   Cube    Square Root Cube Root\n",n);
-    for(i=1;i<=n;i++)
+    int operator;
+    float num1,squr,cube,sqrroot,cubroot;
+	
+    printf("Enter an operator\n1:(For Square)\n2:(For Cube)\n3:(For Squareroot)\n4:(For Cuberoot)");
+	
+    scanf("%d",&operator);
+	
+    switch (operator) 
     {
-        printf("%d \t %ld \t %ld \t %ld \t %.2f\n",i,(i*i),(i*i*i),sqrt((double)i),cbrt((double)i));
+        case 1:
+		
+		//user inputs number //
+		
+        printf("Enter First no.");
+		scanf("%f",&num1);
+		//square operation is executed
+        squr=num1*num1;
+        printf("ans=%f\n",squr);
+        break;
+
+        case 2:
+        printf("Enter First no.");
+        scanf("%f",&num1);
+		
+		//cube of the number//
+		
+        cube=num1*num1*num1;
+        printf("ans=%f\n",cube);
+        break;
+
+        case 3:
+        printf("Enter First no.");
+        scanf("%f",&num1);
+		
+		//square root of the number is found by sqrt function //
+		
+        sqrroot= sqrt(num1);
+        printf("ans=%f\n",sqrroot);
+        break;
+
+        case 4:
+        printf("Enter First no.");
+        scanf("%f",&num1);
+		
+		//pow function is being used to calculaye the cube root//
+		
+        cubroot=pow(num1,1.0/3.0);
+        printf("ans=%f\n",div);
+        break;
+
+       
+
     }
-     
     return 0;
+
 }
