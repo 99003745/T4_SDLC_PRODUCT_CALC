b6dbc93ad3ff17d01c8d53a4e9e02a71324caaf4
99003748 <78853340+99003748@users.noreply.github.com>
2021-02-15 10:38:57 +0530

    header file updated

--- Implementation/inc/roots_cubes.h
+++ Implementation/inc/roots_cubes.h
@@ -8,3 +8,42 @@
 
 #include <stdio.h>
 #include <math.h>
+
+//#include <stdio_ext.h>
+
+/**
+*  squares the num1  and returns the result
+* @param[in] num1 
+* 
+* @return Result of num1 * num1
+*/
+int squr(int num1);
+
+/**
+*  cubes the num1 and returns the result
+* @param[in] num1 
+* 
+* @return Result of num1 * num1 * num1
+*/
+int cube(int num1);
+
+/**
+*  squroot the num1 and returns the result
+* @param[in] num1 
+*  
+* @return Result of sqrt(num1)
+*/
+int squrroot(int num1);
+
+
+/**
+* cuberoot the num1  and returns the result 
+* @param[in] num1 
+* @return Result of pow(num1, 1.0/3.0)
+* 
+* @return integer value of the cube root of num1
+* 
+*/
+int cubroot(int num1);
+
+#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
