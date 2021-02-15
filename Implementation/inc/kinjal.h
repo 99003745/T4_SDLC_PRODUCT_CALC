a42eb618bd2c4b405b5eaeb0ea4fc2454a66cbfd
99003744 <78848744+99003744@users.noreply.github.com>
2021-02-15 13:20:57 +0530

    Create kinjal.h

---
+++ Implementation/inc/kinjal.h
@@ -0,0 +1,11 @@
+#ifndef __KINJAL_H__
+#define __KINJAL_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void bmi(int w,int h);
+void areac(int r);
+void arear(int l,int b);
+void areas(int s);
+#endif 
