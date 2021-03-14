f0acd3029a8d1759a692ea928685ea7a46b41e89
99003748 <78853340+99003748@users.noreply.github.com>
2021-03-14 19:07:57 +0530

    Update roots_cubes.c
    code update

--- 3_Implementation/src/roots_cubes.c
+++ 3_Implementation/src/roots_cubes.c
@@ -1,66 +1,5 @@
 #include <roots_cubes.h>
  
-/*
-int root_cubes()
-{
-    int operator;
-    float num1,squr,cube,sqrroot,cubroot;
-	
-    printf("Enter an operator\n1:(For Square)\n2:(For Cube)\n3:(For Squareroot)\n4:(For Cuberoot)");
-	
-    scanf("%d",&operator);
-	
-    switch (operator) 
-    {
-        case 1:
-		
-		//user inputs number //
-		
-        printf("Enter First no.");
-		scanf("%f",&num1);
-		//square operation is executed
-        squr=num1*num1;
-        printf("ans=%f\n",squr);
-        break;
-
-        case 2:
-        printf("Enter First no.");
-        scanf("%f",&num1);
-		
-		//cube of the number//
-		
-        cube=num1*num1*num1;
-        printf("ans=%f\n",cube);
-        break;
-
-        case 3:
-        printf("Enter First no.");
-        scanf("%f",&num1);
-		
-		//square root of the number is found by sqrt function //
-		
-        sqrroot= sqrt(num1);
-        printf("ans=%f\n",sqrroot);
-        break;
-
-        case 4:
-        printf("Enter First no.");
-        scanf("%f",&num1);
-		
-		//pow function is being used to calculaye the cube root//
-		
-        cubroot=pow(num1,1.0/3.0);
-        printf("ans=%f\n",cubroot);
-        break;
-
-       
-
-    }
-    return 0;
-
-}
-*/
-
 /** 
 * @file calculator_operations.h
 * Calculator application with 4 mathematical operations
@@ -72,10 +11,10 @@ int root_cubes()
 #include <stdio.h>
 #include <math.h>
 
-//#include <stdio_ext.h>
+
 
 /**
-*  squares the num1  and returns the result
+*  squares the number  and returns the result
 * @param[in] num1 
 * 
 * @return Result of num1 * num1
@@ -86,7 +25,7 @@ int square(int num1)
 }
 
 /**
-*  cubes the num1 and returns the result
+*  cubes the number and returns the result
 * @param[in] num1 
 * 
 * @return Result of num1 * num1 * num1
@@ -97,7 +36,7 @@ int cube(int num1)
 }
 
 /**
-*  squroot the num1 and returns the result
+*  squreroot the number and returns the result
 * @param[in] num1 
 *  
 * @return Result of sqrt(num1)
@@ -109,7 +48,7 @@ int squarroot(int num1)
 
 
 /**
-* cuberoot the num1  and returns the result 
+* cuberoot the number  and returns the result 
 * @param[in] num1 
 * @return Result of pow(num1, 1.0/3.0)
 * 
